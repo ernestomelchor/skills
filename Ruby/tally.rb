4fa4e60a6bb7c26bc2971c5bcd0dd498d82fee55
ernestomelchor <nurturelove88@gmail.com>
2020-07-04 15:11:17 -0500

    solution code without use of the .tally ruby method

--- Ruby/tally.rb
+++ Ruby/tally.rb
@@ -11,3 +11,22 @@ def three_strings(strings)
 end
 
 p three_strings(["hi", "hello", "hi", "hi", "hello", "bye", "hello"])
+
+# What if you wanted to solve this problem without using the .tally method above?
+
+def three_strings(strings)
+  result = []
+  tally = {}
+  strings.each do |string|
+    if !tally.include? string
+      tally[string] = 1
+    else
+      tally[string] += 1
+    end
+  end
+  three_plus = tally.select { |k, v| v >= 3 }
+  three_plus.each_key { |key| result << key }
+  return result
+end
+
+p three_strings(["hi", "hello", "meow", "hi", "hi", "meow", "hello", "bye", "hello", "meow"])
