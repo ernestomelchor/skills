81107a49645a7524048ca0c88c32fda9941380ca
ernestomelchor <nurturelove88@gmail.com>
2020-07-08 09:49:07 -0500

    studied converting data types using javascript

--- JavaScript/convert_data_types.js
+++ JavaScript/convert_data_types.js
@@ -38,7 +38,7 @@ console.log(
 );
 
 // 3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
-//    For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+//    For example, "bookkeeper" becomes { b: 1, o: 2, k: 2, e: 3, p: 1, r: 1 }.
 
 function stringToHash(string) {
   let result = {};
@@ -60,7 +60,7 @@ function stringToHash(string) {
 console.log(stringToHash("bookkeeper"));
 
 // 4. Convert a hash into an array of arrays.
-//    For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+//    For example, {"chair" => 100, "book" => 14} becomes [ [ 'chair', 100 ], [ 'book', 14 ] ].
 
 function hashToArrays(hash) {
   let result = [];
@@ -100,11 +100,66 @@ console.log(
 // 6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
 //    For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
 
+function convertToHash(array) {
+  let result = {};
+  array.forEach((word) => {
+    if (!result.hasOwnProperty(word)) {
+      result[word] = 1;
+    } else {
+      result[word]++;
+    }
+  });
+  return result;
+}
+
+console.log(convertToHash(["do", "or", "do", "not"]));
+
 // 7. Convert a hash into a flat array containing all the hash’s keys and values.
-//    For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
+//    For example, { a: 1, b: 2, c: 3, d: 4 } becomes ['a', 1, 'b', 2, 'c', 3, 'd', 4].
+
+// Using Object.entries() method that creates a nested array but then is flattened to a single array using .flat() method
+function convertHashToArray(hash) {
+  return Object.entries(hash).flat();
+}
+
+console.log(convertHashToArray({ a: 1, b: 2, c: 3, d: 4 }));
+
+// Using a for loop to avoid flattening a nested array (ideally reduces amount of computational steps, i.e. runtime)
+function hashToArray(hash) {
+  let result = [];
+  for (let key in hash) {
+    let value = hash[key];
+    result.push(key, value);
+  }
+  return result;
+}
+
+console.log(hashToArray({ a: 1, b: 2, c: 3, d: 4 }));
 
 // 8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
+
 //    For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+
+// This code is not outputting as intended. Will need to review and return to it later.
+function combineData(hash, array) {
+  let result = {};
+  for (let item in hash) {
+    let price = hash[item];
+    for (let element in array) {
+      let color = array[element].color;
+      let weight = array[element].weight;
+      result[item] = { price, color, weight };
+    }
+  }
+  return result;
+}
+
+console.log(
+  combineData({ chair: 75, book: 15 }, [
+    { name: "chair", color: "red", weight: 10 },
+    { name: "book", color: "black", weight: 1 },
+  ])
+);
 
 // 9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 //    For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
